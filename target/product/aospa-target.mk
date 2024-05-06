bd24f997111e4ca36774276b04ce41a8b3940952
Fazil Sheik <fazil.sheik96@gmail.com>
2024-05-06 21:42:13 +0530

    target: Build DesktopMode
    Change-Id: Iba3caa0f5ac2c87a1e4277d311ff912dbe9da9cc

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -57,6 +57,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Debloater
 
+# DesktopMode
+PRODUCT_PACKAGES += \
+    DesktopMode
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
+
+$(call inherit-product-if-exists, packages/services/VncFlinger/product.mk)
+
 # Dex
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed
