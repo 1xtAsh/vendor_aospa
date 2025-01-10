d56f37fdd7592ef8b6c2783e2df4290619ff54c8
Omkar Chandorkar <gotenksIN@aospa.co>
2025-01-10 07:09:35 +0000

    Revert "aospa: Build Paranoid Settings"
    This reverts commit c39a885b89a9f7bb05e308a716d0e6179e42022c.
    
    Reason for revert: Not needed since switch to LMO-Style Settings extension
    
    Change-Id: I78aeda5188c42a5a8fd2708b3ad91b85bdf4d19f

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -195,7 +195,6 @@ $(call inherit-product-if-exists, vendor/aospa/translations/translations.mk)
 # Paranoid Packages
 PRODUCT_PACKAGES += \
     ParanoidPapers \
-    ParanoidSettings \
     ParanoidSystemUI \
     ParanoidThemePicker
 
